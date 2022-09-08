4e4462dd4c03e5aed59c7ad321cc07d62d543960
Jia Wei Loo <huggingrobot@gmail.com>
2022-09-08 17:50:13 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Step from './components/step/step';
+
+function App() {
+  return (
+    <div className="App">
+     <Step todo=""></Step>
+    </div>
+  );
+}
+
+export default App;
