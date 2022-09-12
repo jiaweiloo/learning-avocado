5686ae6cb2a3f5fd12c4f037172a56fe4d19b7a3
Jia Wei Loo <huggingrobot@gmail.com>
2022-09-12 18:21:18 +0800

    Added new lesson

---
+++ src/pages/NewLesson.tsx
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+import "./step.css";
+
+interface props {
+  activeLvl: number;
+}
+
+const Step: React.FC<props> = ({ activeLvl }) => {
+  return <div className="main_body"></div>;
+};
+
+export default Step;
